119da5b6d2f28cfd0133de8e15850c2d59e2db5d
ya <zhangya_998@sina.com>
2014-04-01 02:49:49 +0800

    modify statcounter source

---
+++ src/js/statcounter.js
@@ -0,0 +1,7 @@
+var sc_project = 9712791;
+var sc_invisible = 1;
+var sc_security = "5b91c101";
+var scJsHost = (("https:" == document.location.protocol) ? "https://secure."
+		: "http://www.");
+document.write("<sc" + "ript type='text/javascript' src='" + scJsHost
+		+ "statcounter.com/counter/counter_xhtml.js'></" + "script>");
