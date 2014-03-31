84d297599ab0a22c922bcf4930ba9d4e5e5f70de
ya <zhangya_998@sina.com>
2014-04-01 03:24:26 +0800

    add google analysis

--- src/js/statcounter.js
+++ src/js/statcounter.js
@@ -5,3 +5,17 @@ var scJsHost = (("https:" == document.location.protocol) ? "https://secure."
 		: "http://www.");
 document.write("<sc" + "ript type='text/javascript' src='" + scJsHost
 		+ "statcounter.com/counter/counter_xhtml.js'></" + "script>");
+
+(function(i, s, o, g, r, a, m) {
+	i['GoogleAnalyticsObject'] = r;
+	i[r] = i[r] || function() {
+		(i[r].q = i[r].q || []).push(arguments)
+	}, i[r].l = 1 * new Date();
+	a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+	a.async = 1;
+	a.src = g;
+	m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-49580802-1', 'ustc.edu.cn');
+ga('send', 'pageview');
